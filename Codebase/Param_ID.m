0c4802e32624977fa66b2f67d19d52404aab0ce7
dkkato <dkkato@berkeley.edu>
2019-05-03 16:34:34 -0700

    LARGE CODE CHANGE
    De-complicate the code to mkae ti more clear, removed many of the unnecessary lines

--- Codebase/Param_ID.m
+++ Codebase/Param_ID.m
@@ -48,12 +48,12 @@ np = sum(selection_vector);
 delta_theta_history = ones(np,1)*100;
 
 while exit_logic == false
-    % Reset alpha 
+    % Reset alpha
     alpha = 1e5;
-
+    
     %Sample with replacement
     rand_idx25 = sel_k(randsample(np,groupsize))
-%     rand_idx25 = 25;
+    %     rand_idx25 = 25;
     rand_idx22 = twenty2to25(rand_idx25,'522');
     e_idx = zeros(size(selection_vector));
     e_idx(rand_idx25) = 1;
@@ -64,77 +64,89 @@ while exit_logic == false
     % Pick random coordinate, optimize cost function via approx. line search along that
     % coordinate
     btrk = true;
+    
+    p = update_p(p,theta);
+    
+    V_CELL = cell(num_inputs,1);
+    S_CELL = cell(num_inputs,1);
+    
+    %             parfor idx = 1:num_inputs
+    for idx = 1:num_inputs
+        [V_CELL{idx}, ~, S_CELL{idx}] = DFN_sim_casadi(p,...
+            exp_num{idx+1},Current_exp{idx+1}(1:end), Time_exp{idx+1}(1:end), ...
+            Voltage_exp{idx+1}(1:end), T_amb{idx+1}, e_idx, theta, 1,Rc{idx+1});
+    end
+    
+    v_sim = cell2mat(V_CELL);
+    Sens = cell2mat(S_CELL);
+    costprev = norm(v_dat - v_sim,2);
+    
+    % Normalize Sensitivity
+    Selected_params = theta(sel_k);  % Goes from 25x1 vector to 22x1 (in all params selected scenario)
+    normalized_sens_bar = origin_to_norm('sens',Selected_params,bounds,selection_vector);
+    Jac = bsxfun(@times,normalized_sens_bar(rand_idx25),Sens);
+    
+    alpha =min(1e3 ,1/(Jac')*(v_dat - v_sim));
+    
+    theta_prev = theta; % NOTE: UN-NORMALIZED this is the theta from the previous successful iteration
+    
+    %% Line search
     while btrk
         try
-            p = update_p(p,theta);
-            
-            V_CELL = cell(num_inputs,1);
-            S_CELL = cell(num_inputs,1);
-            
-%             parfor idx = 1:num_inputs
-            for idx = 1:num_inputs
-                [V_CELL{idx}, ~, S_CELL{idx}] = DFN_sim_casadi(p,...
-                    exp_num{idx+1},Current_exp{idx+1}(1:end), Time_exp{idx+1}(1:end), ...
-                    Voltage_exp{idx+1}(1:end), T_amb{idx+1}, e_idx, theta, 1,Rc{idx+1});
-            end
-            
-            v_sim = cell2mat(V_CELL);
-            Sens = cell2mat(S_CELL);
-            costprev = norm(v_dat - v_sim,2);
-            
-            %% Line search
-            while true 
-    %             update parameter, just simulate to see if cost improves, 
-    %             if not reduce alpha,
-    %             if so, break
-
+            while true
+                %             update parameter, just simulate to see if cost improves,
+                %             if not reduce alpha,
+                %             if so, break
+                
                 % DEBUG
-                plot(v_sim) 
+                plot(v_sim)
                 drawnow
-
-                % Normalize Sensitivity
-                Selected_params = theta(sel_k);  % Goes from 25x1 vector to 22x1 (in all params selected scenario)
-                normalized_sens_bar = origin_to_norm('sens',Selected_params,bounds,selection_vector);
-                Jac = bsxfun(@times,normalized_sens_bar(rand_idx25),Sens);
-
-                % Update / increase alpha?   
+                
+%                 % Normalize Sensitivity
+%                 Selected_params = theta(sel_k);  % Goes from 25x1 vector to 22x1 (in all params selected scenario)
+%                 normalized_sens_bar = origin_to_norm('sens',Selected_params,bounds,selection_vector);
+%                 Jac = bsxfun(@times,normalized_sens_bar(rand_idx25),Sens);
+                
+                % Update / increase alpha?
                 delta_theta = alpha*(Jac')*(v_dat - v_sim); % NOTE: THIS UPDATE IS NORMALIZED
                 delta_theta_history(rand_idx22) = delta_theta;
-
+                
                 % Parameter Normalization -- NOTE: NEEDS TESTING
-                theta_prev = theta; % NOTE: UN-NORMALIZED
+                %theta_prev = theta; % NOTE: UN-NORMALIZED this is the theta from the previous successful iteration
                 theta_norm = origin_to_norm('param',Selected_params,bounds,selection_vector);
                 theta_norm(rand_idx22) = theta_norm(rand_idx22) + delta_theta;
                 theta_norm(rand_idx22) = min(max(0,theta_norm(rand_idx22)),1); % min max routine prevents parameter value from violating bounds
-                theta = norm_to_origin(theta_norm,bounds,selection_vector); 
-
+                theta = norm_to_origin(theta_norm,bounds,selection_vector);
+                
                 %check if we should got to another parameter or update this one
                 %again
                 V_CELL = cell(num_inputs,1);
                 S_CELL = cell(num_inputs,1);
-
+                
                 for idx = 2%1:num_inputs
-                [V_CELL{idx-1}] = DFN_sim_casadi(p,...
+                    [V_CELL{idx-1}] = DFN_sim_casadi(p,...
                         exp_num{idx},Current_exp{idx}(1:end), Time_exp{idx}(1:end), ...
                         Voltage_exp{idx}(1:end), T_amb{idx}, e_idx, theta, 0,Rc{idx});
                 end
                 v_new = cell2mat(V_CELL);
                 costnew = norm(v_dat - v_new,2)
-
+                
                 %check if the cost got worse
                 if costnew > costprev
                     %reset theta
                     theta = theta_prev;
                     %reduce step size
-                    alpha = alpha/10;
+                    grad = (Jac')*(v_dat - v_sim);
+                    alpha = min(alpha,1/abs(grad));
+                    alpha = alpha/2;
                 else
-                     %It will reach this when a step improves the cost
+                    %It will reach this when a step improves the cost
                     btrk = false;
                     break
                 end
             end
             
-           
+            
             
         catch e % logic for when casadi fails %TEST THIS
             % An error will put you here.
@@ -142,20 +154,23 @@ while exit_logic == false
             
             % Propose a smaller parameter step
             alpha = alpha/2; % NOTE: NEEDS TESTING
-            delta_theta = alpha*(Jac')*(v_dat - v_sim); % NOTE: THIS UPDATE IS NORMALIZED
+            %delta_theta = alpha*(Jac')*(v_dat - v_sim); % NOTE: THIS UPDATE IS NORMALIZED
+            %grad = (Jac')*(v_dat - v_sim);
+            %alpha = min(alpha,1/abs(grad));
             
             % Parameter Normalization -- NOTE: NEEDS TESTING
-            theta_norm = origin_to_norm('param',Selected_params,bounds,selection_vector);
-            theta_norm(rand_idx22) = theta_prev(rand_idx22) + delta_theta;
-            theta_norm(rand_idx22) = min(max(0,theta_norm(rand_idx22)),1); % min max routine prevents parameter value from violating bounds
-            theta = norm_to_origin(theta_norm,bounds,selection_vector); 
+            %theta_norm = origin_to_norm('param',Selected_params,bounds,selection_vector);
+            %theta_norm(rand_idx22) = theta_prev(rand_idx22) + delta_theta;
+            %theta_norm(rand_idx22) = min(max(0,theta_norm(rand_idx22)),1); % min max routine prevents parameter value from violating bounds
+            %theta = norm_to_origin(theta_norm,bounds,selection_vector);
+            theta = theta_prev;
         end
         
         
     end
     
     % Check Exit Conditions
-%     [exit_logic] = check_ec(v_dat,v_sim,delta_theta_history,Iter,SCD_options);
+    %     [exit_logic] = check_ec(v_dat,v_sim,delta_theta_history,Iter,SCD_options);
     
     % Save ParamID results every iteration
     %     paramID_out.Time_exp = Time_exp;
